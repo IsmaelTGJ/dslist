dbbb8b2b4207643cbd4cf9249ee31dbec3200c71
IsmaelTGJ <ismaeltgj@hotmail.com>
2025-05-23 21:01:17 -0300

    GET Game by Id

--- src/main/java/com/devitga/dslist/controllers/GameController.java
+++ src/main/java/com/devitga/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devitga.dslist.dto.GameDTO;
 import com.devitga.dslist.dto.GameMinDTO;
 import com.devitga.dslist.services.GameService;
 
@@ -20,6 +22,14 @@ public class GameController {
 	@GetMapping
 	public List<GameMinDTO> findAll(){
 		List<GameMinDTO> result = gameService.findAll();
+		
+		return result;
+		
+	}
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id){
+		GameDTO result = gameService.findById(id);
 		
 		return result;
 		
